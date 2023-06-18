af1a5ed9cf297a13783e84a704a981031f7d9bc0
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-06-18 10:00:47 +0300

    Revert "default page allocations"
    This reverts commit b5c7a9280a6d132c630a4dfdfe73f35ca5e1382a.

--- internal/os.h
+++ internal/os.h
@@ -28,23 +28,6 @@ void free_page(void* lpAddres,DWORD size,DWORD freeType)
 
 }
 
-void* allocate_page_default(void* lpAddress, std::size_t size)
-{
-    return ::VirtualAlloc
-    (
-        nullptr,
-        size,
-        MEM_RESERVE | MEM_COMMIT,
-        PAGE_READWRITE
-    );
-}
-
-void free_page_default(void* lpAddres, DWORD size)
-{
-    ::VirtualFree((void*)(lpAddres), size, MEM_FREE);
-
-}
-
 #else
 #error NOT DEFINED FOR THIS PLATFORM
 #endif
